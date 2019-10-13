998066f303eb8a5da1ade2e7b4d6389867ee798f
PiotrK <pkowalik98@gmail.com>
2019-10-13 15:00:17 +0200

    lab1ex2

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import AppTitle from './AppTitle'
+
 const MyApp = () => (
   <div>
     <AppTitle />
@@ -7,5 +8,8 @@ const MyApp = () => (
     <p>Bundle size: 58 bytes, Load time of the bundle: 52 ms, Last commit: Customize headline</p>
   </div>
 )
+
+const generateArray = (n) =>( Array.from(Array(n),(x,index)=>index+1) )
+console.log(generateArray(10));
 
 export default MyApp
