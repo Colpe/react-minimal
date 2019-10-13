eaf826bd5d43f65a60082ffa1e2cfd73c979837a
PiotrK <pkowalik98@gmail.com>
2019-10-13 12:07:22 +0200

    Production bundle analysis

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const MyApp = () => (
-  <h1>Minimal React delimatj</h1>  
+  <div>
+    <h1>Minimal React delimatj</h1>
+    <p>Bundle size: 58 bytes, Load time of the bundle: 52 ms, Last commit Customize headline: Z</p>
+  </div>
+
 )
 
 export default MyApp
