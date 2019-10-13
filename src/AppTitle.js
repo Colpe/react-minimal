5520aae23280374d56de5a6a3bd5840031ca9d2d
PiotrK <pkowalik98@gmail.com>
2019-10-13 15:09:26 +0200

    lab1ex3

--- src/AppTitle.js
+++ src/AppTitle.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const AppTitle = () => (
   <div>
     <h1>App Title</h1>
--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import AppTitle from './AppTitle'
+import { randomBytes } from 'crypto';
 
 const MyApp = () => (
   <div>
@@ -11,5 +12,11 @@ const MyApp = () => (
 
 const generateArray = (n) =>( Array.from(Array(n),(x,index)=>index+1) )
 console.log(generateArray(10));
+
+
+const generateRandomArray = (n) =>( Array.from(Array(n),(x,index)=>Math.ceil(Math.random()*24 +1)) )
+
+console.log(generateRandomArray(10));
+
 
 export default MyApp
